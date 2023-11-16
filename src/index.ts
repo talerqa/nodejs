b7f8d30b3ca6ed1fd376d0422c0b454d92c87079
Aliaksei <talerqa@gmail.com>
2023-11-16 14:12:02 +0300

    fix index js

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import express, {Request, Response} from 'express'
 
 const app = express()
-const port = 3000
+const port = process.env.PORT || 5000
 
 app.get('/', (req: Request, res: Response) => {
   let helloMessage = '!!!!!!!'
