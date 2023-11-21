8699801b8559b740458e9697dbd252cb3273c629
Aliaksei <talerqa@gmail.com>
2023-11-21 19:23:01 +0300

    feat: fix port

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@ import {productsRoute} from "./routes/products_routes";
 import {addressesRoute} from "./routes/addreses_routes";
 
 const app = express()
-const port = process.env.PORT || '5000'
+const port = process.env.PORT || '8080'
 
 const parserMiddleWare = bodyParser({})
 app.use(parserMiddleWare)
