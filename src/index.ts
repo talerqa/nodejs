ecc734dbe4ab439bfe9d148420a1d13d887a07a7
Aliaksei <talerqa@gmail.com>
2023-12-02 16:19:55 +0300

    fix: cors

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import bodyParser from "body-parser";
+import cors from "cors";
 import {productsRoute} from "./routes/products_routes";
 import {addressesRoute} from "./routes/addreses_routes";
 
@@ -8,30 +9,12 @@ const port = process.env.PORT || 5000
 
 const parserMiddleWare = bodyParser({})
 
+app.use(cors({ credentials: true }))
 app.use(parserMiddleWare)
 
 app.get("/", function (request, response) {
   response.send("<h2>Products</h2>");
 });
-
-
-const allowCors = (fn: any) => async (req: any, res: any) => {
-  res.setHeader('Access-Control-Allow-Credentials', true)
-  res.setHeader('Access-Control-Allow-Origin', '*')
-  // another common pattern
-  // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
-  res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
-  res.setHeader(
-    'Access-Control-Allow-Headers',
-    'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
-  )
-  if (req.method === 'OPTIONS') {
-    res.status(200).end()
-    return
-  }
-  return await fn(req, res)
-}
-
 
 app.use('/products', productsRoute)
 app.use('/addresses', addressesRoute)
@@ -40,4 +23,3 @@ app.listen(port, () => {
   console.log(port)
 })
 
-module.exports = allowCors(app)
